19cc46de5bb19377be863a5a1163e8e71d133a07
Stefanie Dao <ctdao@ucsd.edu>
2020-10-25 11:05:51 -0700

    Initial Commit

---
+++ Target Support Files/Pods-codepath_hw1-codepath_hw1UITests/Pods-codepath_hw1-codepath_hw1UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_codepath_hw1_codepath_hw1UITests : NSObject
+@end
+@implementation PodsDummy_Pods_codepath_hw1_codepath_hw1UITests
+@end
---
+++ Target Support Files/Pods-codepath_hw1Tests/Pods-codepath_hw1Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_codepath_hw1Tests : NSObject
+@end
+@implementation PodsDummy_Pods_codepath_hw1Tests
+@end
